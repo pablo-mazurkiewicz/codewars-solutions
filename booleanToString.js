0bfa622a388118709d090dab966fcea877c5fa7b
pablo-mazurkiewicz <pmazurkiewicz01@gmail.com>
2025-06-30 18:12:39 +0200

    BooleanToString solution

---
+++ booleanToString.js
@@ -0,0 +1,14 @@
+function booleanToString(value) {
+//   if (value === true) {
+//     return 'true';
+//   }
+//   return 'false';
+
+//  return value ? 'true' : 'false';
+
+return value.toString()
+}
+
+// Example usage:
+console.log(booleanToString(true));  // Output: 'true'
+console.log(booleanToString(false)); // Output: 'false'
