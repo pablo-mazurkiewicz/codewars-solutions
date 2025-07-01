27c222e5d65487dfba829ccef352a1c33d15d9d0
pablo-mazurkiewicz <pmazurkiewicz01@gmail.com>
2025-07-01 12:10:12 +0200

    corrected two sum

--- twoSum.js
+++ twoSum.js
@@ -1,9 +1,15 @@
+//leetcode problem: https://leetcode.com/problems/two-sum/
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
 function twoSum(nums, target) {
     const seen = {};
-    for (let i = 0; i < length.nums; i++) {
+    for (let i = 0; i < nums.length; i++) {
         const needed = target - nums[i];
         if (seen[needed] !== undefined) {
             return [seen[needed], i]
         }
+        seen[nums[i]] = i;
     }
 }
